780464e142c59536bbdb2cea8dd43131471fbe2d
Peter Freitas <freitaspeteroliveira@gmail.com>
2021-11-09 10:06:45 -0300

    Criação model

--- src/main/java/com/peterfreitas/heroesapi/config/HeroesTable.java
+++ src/main/java/com/peterfreitas/heroesapi/config/HeroesTable.java
@@ -25,7 +25,7 @@ public class HeroesTable {
 
         DynamoDB dynamoDB = new DynamoDB(client);
 
-        String tableName = "Heroes_Api_Table";
+        String tableName = "Heroes_Table";
 
         try {
             Table table = dynamoDB.createTable(tableName,
